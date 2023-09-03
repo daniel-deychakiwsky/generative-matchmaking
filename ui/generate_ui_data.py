1f4704704f571da2104733bce29e3f6e8ba4e82a
dimitri deychakiwsky <dimitri.deychak@gmail.com>
2023-09-02 23:58:47 -0500

    script

---
+++ ui/generate_ui_data.py
@@ -0,0 +1,48 @@
+import json
+import os
+import base64
+
+# Define the directory where the profiles are stored
+profiles_dir = '../profiles'
+
+# List all the profile IDs (directories) under the profiles directory
+profile_ids = os.listdir(profiles_dir)
+
+# Initialize an empty dictionary to store all profile data
+all_profiles = {}
+
+# Loop through all profile IDs to read their respective data
+for profile_id in profile_ids:
+    profile_data = {}
+
+    # Construct directory path for each profile
+    profile_dir = os.path.join(profiles_dir, profile_id)
+
+    # Read profile.json
+    with open(os.path.join(profile_dir, 'profile.json'), 'r') as f:
+        profile_data['profile'] = json.load(f)
+
+    # Read matches.json
+    with open(os.path.join(profile_dir, 'matches.json'), 'r') as f:
+        profile_data['matches'] = json.load(f)
+
+    # Check if profile.png exists and if so, read it as a base64 encoded string
+    profile_image_path = os.path.join(profile_dir, 'profile.png')
+    if os.path.exists(profile_image_path):
+        with open(profile_image_path, 'rb') as f:
+            base64_image = base64.b64encode(f.read()).decode('utf-8')
+            profile_data['image'] = base64_image
+
+    # Add profile data to all_profiles dictionary
+    all_profiles[profile_id] = profile_data
+
+# Convert the dictionary to a JSON-formatted string
+all_profiles_json_str = json.dumps(all_profiles)
+
+# Create a JavaScript file that contains the profiles data
+js_content = f'export const profiles = {all_profiles_json_str};'
+
+# Save this JavaScript content to a .js file
+js_file_path = './src/all_profiles.js'
+with open(js_file_path, 'w') as f:
+    f.write(js_content)
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,23 +1,26 @@
-import './App.css';
-import { useMemo, useState, useEffect } from 'react';
-import axios from 'axios';
-import { profiles as allProfiles } from './all_profiles'
+import "./App.css";
+import { useMemo, useState, useEffect } from "react";
+import axios from "axios";
+import { profiles as allProfiles } from "./all_profiles";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const profiles = Object.entries(allProfiles).map(([key, value]) => ({ profileKey: key, profileData: value }));
+const profiles = Object.entries(allProfiles).map(([key, value]) => ({
+  profileKey: key,
+  profileData: value,
+}));
 
 const base64ToImage = (base64String) => {
-  return `data:image/png;base64,${base64String}`
-}
+  return `data:image/png;base64,${base64String}`;
+};
 
-const flattenObjectToRows = (object, prefix = '') => {
+const flattenObjectToRows = (object, prefix = "") => {
   let rows = [];
   for (const [key, value] of Object.entries(object)) {
     const fullKey = prefix ? `${prefix}.${key}` : key;
-    if (typeof value === 'object') {
+    if (typeof value === "object") {
       if (Array.isArray(value)) {
-        rows.push({ key: fullKey, value: value.join(', ') });
+        rows.push({ key: fullKey, value: value.join(", ") });
       } else {
         rows = rows.concat(flattenObjectToRows(value, fullKey));
       }
@@ -28,92 +31,165 @@ const flattenObjectToRows = (object, prefix = '') => {
   return rows;
 };
 
-
 const ProfileAvatar = ({ src, name, selected, onSelect }) => (
   <div
     style={{
-      cursor: 'pointer',
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'flex-start',
+      cursor: "pointer",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "flex-start",
     }}
     onMouseEnter={onSelect}
   >
     <div
       style={{
-        borderRadius: '50%',
-        width: '170px',
-        height: '170px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        border: selected ? '3px solid black' : '3px solid transparent',
-        boxSizing: 'border-box',
-        marginBottom: '10px',
-        boxShadow: selected ? '0px 0px 10px 5px rgba(0, 0, 0, 0.2)' : 'none',  // Added black box-shadow
-        backgroundColor: selected ? 'rgba(0, 0, 0, 0.1)' : 'transparent',  // Added black background color
+        borderRadius: "50%",
+        width: "170px",
+        height: "170px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        border: selected ? "3px solid black" : "3px solid transparent",
+        boxSizing: "border-box",
+        marginBottom: "10px",
+        boxShadow: selected ? "0px 0px 10px 5px rgba(0, 0, 0, 0.2)" : "none", // Added black box-shadow
+        backgroundColor: selected ? "rgba(0, 0, 0, 0.1)" : "transparent", // Added black background color
       }}
     >
       <img
         style={{
-          borderRadius: '50%',
-          width: '100%',
-          height: '100%',
-          objectFit: 'cover',
+          borderRadius: "50%",
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
         }}
         src={src}
         alt={name}
       />
     </div>
-    <p style={{
-      fontWeight: selected ? 'bold' : 'normal',
-      textAlign: 'center',
-      maxWidth: '180px',
-      wordWrap: 'break-word',
-    }}>
+    <p
+      style={{
+        fontWeight: selected ? "bold" : "normal",
+        textAlign: "center",
+        maxWidth: "180px",
+        wordWrap: "break-word",
+      }}
+    >
       {name}
     </p>
   </div>
 );
 
 const ProfileSection = ({ profileData }) => {
-  const matches = profileData.matches.ranked.map((matchProfileId) => profiles.find(({ profileKey }) => profileKey === matchProfileId));
+  const matches = profileData.matches.candidate_user_ids.map((matchProfileId) =>
+    profiles.find(({ profileKey }) => profileKey === matchProfileId)
+  );
   const [selectedMatch, setSelectedMatch] = useState(null);
-  const selectedMatchProfile = selectedMatch ? profiles.find((profile) => profile.profileKey === selectedMatch).profileData.profile : {};
+  const selectedMatchProfile = selectedMatch
+    ? profiles.find((profile) => profile.profileKey === selectedMatch)
+      .profileData.profile
+    : {};
 
   const profileRows = flattenObjectToRows(profileData.profile);
   const matchRows = flattenObjectToRows(selectedMatchProfile);
 
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gridColumnGap: '100px', alignItems: 'flex-start', paddingBottom: '10px' }}>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'center', padding: '20px' }}>
-        <h3><b>Profile</b></h3>
-        <ProfileAvatar src={base64ToImage(profileData.image)} name={profileData.profile.name} selected={true} />
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "1fr 1fr",
+        gridColumnGap: "100px",
+        alignItems: "flex-start",
+        paddingBottom: "10px",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          justifyContent: "center",
+          padding: "20px",
+        }}
+      >
+        <h3>
+          <b>Profile</b>
+        </h3>
+        <ProfileAvatar
+          src={base64ToImage(profileData.image)}
+          name={profileData.profile.name}
+          selected={true}
+        />
       </div>
-      <div style={{ padding: '20px' }}>
-        <h3><b>Ranked Matches</b></h3>
-        <div style={{ display: 'flex', flexDirection: 'row', gap: '20px', alignItems: 'center', height: '200px' }}>
+      <div style={{ padding: "20px" }}>
+        <h3>
+          <b>Matches</b>
+        </h3>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "20px",
+            alignItems: "center",
+            height: "250px",
+            width: "1200px",
+            overflow: "auto",
+          }}
+        >
           {matches.map((match) => (
-            <ProfileAvatar key={match.profileKey} src={base64ToImage(match.profileData.image)} name={match.profileData.profile.name} selected={selectedMatch === match.profileKey} onSelect={() => setSelectedMatch(match.profileKey)} />
+            <ProfileAvatar
+              key={match.profileKey}
+              src={base64ToImage(match.profileData.image)}
+              name={match.profileData.profile.name}
+              selected={selectedMatch === match.profileKey}
+              onSelect={() => setSelectedMatch(match.profileKey)}
+            />
           ))}
         </div>
       </div>
-      <div style={{ gridColumn: '1 / 3' }}>
-        <table style={{ borderCollapse: 'collapse', width: '100%' }}>
+      <div style={{ gridColumn: "1 / 3" }}>
+        <table style={{ borderCollapse: "collapse", width: "100%" }}>
           <thead>
             <tr>
-              <th style={{ border: '1px solid black', padding: '8px' }}><b>Attribute</b></th>
-              <th style={{ border: '1px solid black', padding: '8px', width: '50%' }}><b>Profile Value</b></th>
-              <th style={{ border: '1px solid black', padding: '8px', width: '50%' }}><b>Match Value</b></th>
+              <th style={{ border: "1px solid black", padding: "8px" }}>
+                <b>Attribute</b>
+              </th>
+              <th
+                style={{
+                  border: "1px solid black",
+                  padding: "8px",
+                  width: "50%",
+                }}
+              >
+                <b>Profile Value</b>
+              </th>
+              <th
+                style={{
+                  border: "1px solid black",
+                  padding: "8px",
+                  width: "50%",
+                }}
+              >
+                <b>Match Value</b>
+              </th>
             </tr>
           </thead>
           <tbody>
             {profileRows.map((row, index) => (
-              <tr key={index} style={{ borderBottom: '1px solid black' }}>
-                <td style={{ border: '1px solid black', padding: '8px' }}>{row.key}</td>
-                <td style={{ border: '1px solid black', padding: '8px' }}>{String(row.value)}</td>
-                <td style={{ border: '1px solid black', padding: '8px' }}>{String(matchRows.find((matchRow) => matchRow.key === row.key)?.value || '')}</td>
+              <tr key={index} style={{ borderBottom: "1px solid black" }}>
+                <td style={{ border: "1px solid black", padding: "8px" }}>
+                  {row.key}
+                </td>
+                <td style={{ border: "1px solid black", padding: "8px" }}>
+                  {String(row.value)}
+                </td>
+                <td style={{ border: "1px solid black", padding: "8px" }}>
+                  {String(
+                    matchRows.find((matchRow) => matchRow.key === row.key)
+                      ?.value || ""
+                  )}
+                </td>
               </tr>
             ))}
           </tbody>
@@ -122,8 +198,6 @@ const ProfileSection = ({ profileData }) => {
     </div>
   );
 };
-
-
 
 function App() {
   return (
